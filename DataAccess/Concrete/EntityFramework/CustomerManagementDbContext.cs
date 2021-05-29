66d8294ceb3b5c7659554519bd3b9346208b6b0b
misraerol <misraerol@gmail.com>
2021-05-29 15:44:13 +0300

    Crud işlemleri yapıldı. Veri tabanı ile bağlandı.

---
+++ DataAccess/Concrete/EntityFramework/CustomerManagementDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class CustomerManagementDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=CustomerManagementDb;
+Trusted_Connection=true");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
